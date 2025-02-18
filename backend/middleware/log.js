ba6f1327950cc8536f810e0fdaea6d26b9444d12
amine ghannouchi <amineghannouchi111@gmail.com>
2025-02-18 19:48:03 +0100

    add a witelist for request from other serves and handel error

--- backend/middleware/log.js
+++ backend/middleware/log.js
@@ -5,11 +5,10 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message) => {
+const logEvents = async (message, logeDate = format(new Date(), 'yyyyMMdd')) => {
     const Time = `${format(new Date(), 'HH:mm:ss')}`;
-    const logeDate = format(new Date(), 'yyyyMMdd');
     const logItem = `${Time}\t${message}\n`;
-
+    
     try {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const {logger,logEvents} = require('./middleware/log');
+const errorHandler = require('./middleware/errorHandler');
 const port = process.env.PORT || 3500;
 const frontend = path.join(__dirname,'..','frontend','dist');
 //"npm run build" pour cree le dossier 'dist'
@@ -12,6 +14,22 @@ const frontend = path.join(__dirname,'..','frontend','dist');
 
 //custom middleware logger
 app.use(logger);
+
+//cross origin resource sharing
+const whiltelist = ['sql url','frontend url'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whiltelist.indexOf(origin) !== -1 || !origin){// remove !origin from the list
+            callback(null,true);
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions)); //for api from externel websites
+
+app.use(errorHandler);
 
 //^ ← must start with the simbol next to the ^ 
 //$ ← must end with the simbol befor the $
